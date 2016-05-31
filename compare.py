b6b9b2c15105d9daadfaf9684dfaacfd1387c05d
Qluxzz <andre.eric.andersson@gmail.com>
2016-05-31 19:59:35 +0200

    Add loop through stores and compare assortment to each other

--- compare.py
+++ compare.py
@@ -17,8 +17,24 @@ products = {}
 difference = []
 
 for store in stores:
-  r = requests.get(store)
-  if r.status_code == 200:
-    for product in r.json()['ProductSearchResults']:
-      pp.pprint(product['ProductNumber'])
-  print('--------------------------')    r = requests.get('http://www.systembolaget.se/api/productsearch/search?subcategory=%C3%96l&type=Ale&sortdirection=Ascending&fullassortment=0&site=' + str(store) + '&page=' + str(page))
+  page = 0
+  next_page = 0
+  while next_page != -1:
+    r = requests.get('http://www.systembolaget.se/api/productsearch/search?subcategory=%C3%96l&type=Ale&sortdirection=Ascending&fullassortment=0&site=' + str(store) + '&page=' + str(page))
+    if r.status_code == 200:
+      next_page = r.json()['Metadata']['NextPage']
+      page += 1
+      for product in r.json()['ProductSearchResults']:
+        if store == stores[1]:
+          if product['ProductNumber'] not in products:
+             difference.append(product)
+        else:
+          if product['ProductNumber'] not in products:
+             products[product['ProductNumber']] = product
+            
+print('--------- Difference ---------')  
+for product in difference:
+  print('Product')
+  print('Name: ' + product['ProductNameBold'])
+  if 'ProductNameThin' in product != None:
+    print(' Category: ' + str( product['ProductNameThin'] ))
