02f48882e5b11e40acc8e6a273cd5ea3db39d579
Qluxzz <andre.eric.andersson@gmail.com>
2016-06-13 23:35:42 +0200

    Update sent variables to include store info

--- compare.py
+++ compare.py
@@ -4,11 +4,14 @@ import requests
 import json
 from flask import Flask, render_template, request, url_for, redirect, abort
 import xml.etree.ElementTree as ET
+import pprint
 
 app = Flask(__name__)
 
 tree = ET.parse('stores.xml')
 root = tree.getroot()
+
+stores = []
 
 def get_store_info( store_id ):
     for store in stores:
@@ -22,7 +25,6 @@ def remove_duplicates( store1, store2 ):
                 store1.remove(product)
                 store2.remove(product2)
 def get_stores():
-    stores = []
     for child in root.iter('ButikOmbud'):
         # Check if store and not pick up point
         if '-' not in child[1].text:
@@ -36,9 +38,8 @@ def get_stores():
     return sorted( stores, key=lambda store: store['place'])
 def compare_stores( store_one_id, store_two_id ):
     stores = [ store_one_id, store_two_id ]
-
-    products = {}
-    difference = []
+    store_assortment = []
+    current_store = 0
 
     for store in stores:
         products = []
@@ -59,17 +60,23 @@ def compare_stores( store_one_id, store_two_id ):
         remove_duplicates( store_assortment[1], store_assortment[0] )
     return store_assortment
 
+pp = pprint.PrettyPrinter(indent=4)
+
 @app.route('/')
 def index(stores=None):
-    return render_template('index.html', stores = get_stores())
-
-@app.route('/results', methods=['GET', 'POST'])
-def results( products = None ):
-    return render_template('results.html', stores = get_stores())
+    return render_template('index.html', stores = get_stores() )
 
 @app.route('/add_stores', methods=['GET', 'POST'])
-def add_stores():
-    return render_template('results.html', products = compare_stores( request.args.get('store1_field'), request.args.get('store2_field')))
+def add_stores(stores = None, store1_info = None, store2_info = None):
+    store1_id = request.args.get('store1_field')
+    store2_id = request.args.get('store2_field')
+    return render_template(
+        'results.html',
+        stores = compare_stores( store1_id, store2_id),
+        store1_info = get_store_info(store1_id),
+        store2_info = get_store_info(store2_id)
+    )
 
 if __name__ == "__main__":
+    stores = get_stores()
     app.run(debug=True)
