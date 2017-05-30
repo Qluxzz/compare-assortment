92f7353fc0cd727d5b46ab2a930c2b324464fcb7
Qluxzz <andre.eric.andersson@gmail.com>
2017-05-30 05:36:54 +0200

    Refactor logic

--- compare.py
+++ compare.py
@@ -1,23 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: latin-1 -*-
+
 import requests
 from flask import Flask, render_template, request, url_for, redirect, abort
 import xml.etree.ElementTree as ET
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
-app = Flask(__name__)
 
-tree = ET.parse('stores.xml')
-root = tree.getroot()
+def get_url(store):
+    """ Return formated URL """
+    params = {
+        'subcategory': '%C3%96l',
+        'sortdirection': 'Ascending',
+        'fullassortment': '0',
+        'site': str(store),
+        'page': '0'
+    }
+    return 'http://www.systembolaget.se/api/productsearch/search', params
 
-def get_store_info( store_id ):
-    stores = get_stores()
+def get_store_info(stores, store_id):
+    """ Return specific store info """
     for store in stores:
         if store['nr'] == store_id:
             pp.pprint(store)
             return store
-def remove_duplicates( store1, store2 ):
+
+def remove_duplicates(store1, store2):
+    """ Remove duplicates in each store """
     copy_of_store2 = list(store2)
     for product in store1:
         for product2 in store2:
@@ -27,7 +35,11 @@ def remove_duplicates( store1, store2 ):
         for product2 in store1:
             if product['ProductId'] == product2['ProductId']:
                 store1.remove(product2)
+
 def get_stores():
+    """ Parse store information from xml file and return sorted list """
+    tree = ET.parse('stores.xml')
+    root = tree.getroot()
     stores = []
     for child in root.iter('ButikOmbud'):
         # Check if store and not pick up point
@@ -40,48 +52,58 @@ def get_stores():
                 store['name'] = child[2].text
             else:
                 store['name'] = child[3].text
-            stores.append( store )
-    return sorted( stores, key=lambda store: store['place'])
-def compare_stores( store_one_id, store_two_id ):
-    stores = [ store_one_id, store_two_id ]
+            stores.append(store)
+    return sorted(stores, key=lambda store: store['place'])
+
+def compare_stores(store_one_id, store_two_id):
+    """ Compare the products between two stores """
+    store_assortment = get_products([store_one_id, store_two_id])
+
+    if len(store_assortment[0]) > len(store_assortment[1]):
+        remove_duplicates(store_assortment[0], store_assortment[1])
+    else:
+        remove_duplicates(store_assortment[1], store_assortment[0])
+    return store_assortment
+
+def get_products(stores):
+    """ Get products from stores """
     store_assortment = []
-    current_store = 0
 
     for store in stores:
         products = []
-        page = 0
-        next_page = 0
-        while next_page != -1:
-            r = requests.get('http://www.systembolaget.se/api/productsearch/search?subcategory=%C3%96l&sortdirection=Ascending&fullassortment=0&site=' + str(store) + '&page=' + str(page))
-            if r.status_code == 200:
-                next_page = r.json()['Metadata']['NextPage']
-                page += 1
-                for product in r.json()['ProductSearchResults']:
+
+        url, params = get_url(store)
+
+        while params['page'] != -1:
+            _request = requests.get(url, params=params)
+
+            if _request.status_code == 200:
+                data = _request.json()
+                params['page'] = data['Metadata']['NextPage']
+
+                for product in data['ProductSearchResults']:
                     products.append(product)
-        store_assortment.append( products )
-    print(len(store_assortment[0]), len(store_assortment[1]))
-    if len(store_assortment[0]) > len(store_assortment[1]):
-        remove_duplicates( store_assortment[0], store_assortment[1] )
-    else:
-        remove_duplicates( store_assortment[1], store_assortment[0] )
-    print(len(store_assortment[0]), len(store_assortment[1]))
+
+        store_assortment.append(products)
     return store_assortment
+
+app = Flask(__name__)
+app.config['STORES'] = get_stores()
 
 @app.route('/')
 def index(stores=None):
-    return render_template('index.html', stores = get_stores() )
+    return render_template('index.html', stores=app.config['STORES'])
 
 @app.route('/add_stores', methods=['GET', 'POST'])
-def add_stores(stores = None, store1_info = None, store2_info = None):
-    store1_id = request.args.get('store1_field')
-    store2_id = request.args.get('store2_field')
+def add_stores(stores=None, store1_info=None, store2_info=None):
+    store1_id = request.args.get('store1')
+    store2_id = request.args.get('store2')
     return render_template(
         'results.html',
-        stores = compare_stores( store1_id, store2_id),
-        store1_info = get_store_info(store1_id),
-        store2_info = get_store_info(store2_id)
+        stores=compare_stores(store1_id, store2_id),
+        store1_info=get_store_info(app.config['STORES'], store1_id),
+        store2_info=get_store_info(app.config['STORES'], store2_id)
     )
 
 if __name__ == "__main__":
-    stores = get_stores()
     app.run(debug=True)
