54dba5e92c822ce3202de767203e03d6c8dbc057
Qluxzz <andre.eric.andersson@gmail.com>
2017-09-07 21:53:09 +0200

    Move web related stuff out of jinja static folder

---
+++ js/systembolaget.js
@@ -0,0 +1,141 @@
+var products = {}
+var info = {}
+
+const loadData = () => {
+    $.get({
+        url: 'products/5.json'
+    }).done(ps => {
+        console.log(ps)
+        products = ps
+    })
+
+    $.get({
+        url: 'info.json'
+    }).done(data => info = data)
+}
+
+const getStores = () => {
+    var selects = [document.getElementById('store1'), document.getElementById('store2')]
+
+    $.get({
+        url: 'stores/info.json',
+        contentType: 'json'
+    }).done(stores => {
+        stores.forEach(store => {
+            const [nr, name, city] = store
+            
+            selects.forEach(select => {
+                var option = document.createElement('option')
+                option.value = nr
+                option.text = `${name} - ${city}`
+                select.appendChild(option)
+            })
+        })
+    })
+}
+
+const addEventListener = () => {
+    document.getElementById('compare').addEventListener('click', getStoreStock)
+}
+
+var storeStock = []
+
+const getStoreStock = event => {
+    const selectedStores = [
+        parseInt(document.getElementById('store1').value),
+        parseInt(document.getElementById('store2').value)
+    ]
+    storeStock = []
+    selectedStores.forEach(store => getStock(store))
+}
+
+Array.prototype.diff = a => {
+    return this.filter(i => a.indexOf(i) < 0)
+}
+
+var storeDiffStock = []
+
+const getProductInformation = ids => {
+    var stores = []
+    
+    ids.forEach(diff => {
+        var store = []
+        diff.forEach(id => store.push(products[id]))
+        stores.push(store)
+    })
+    viewProductDiff(stores)
+}
+
+const viewProductDiff = stores => {
+    const store1 = document.getElementById('store1Result')
+    const store2 = document.getElementById('store2Result')
+
+    while (store1.hasChildNodes())
+        store1.removeChild(store1.lastChild)
+
+    while (store2.hasChildNodes())
+        store2.removeChild(store2.lastChild)
+
+    stores[0].forEach(product => addProduct(product, store1))
+    stores[1].forEach(product => addProduct(product, store2))
+}
+
+const addProduct = (product, elem) => {
+    const p = document.createElement('div')
+    p.classList.add('col-sm-4')
+
+    const p2 = document.createElement('div')
+    p2.classList.add('product')
+
+    const [name, name2, price, url, country, format, volume, style, type] = product
+
+    const link = document.createElement('a')
+    link.href = `https://systembolaget.se/dryck/${url}`
+
+    const pName = document.createElement('div')
+    pName.classList.add('product-name')
+    pName.innerText += name
+
+    const pText = document.createElement('div')
+    pText.classList.add('product-text')
+    pText.innerHTML += `
+        ${info.countries[country]}<br />
+        ${info.styles[style]}<br />
+        ${info.types[type]}
+    `
+
+    link.appendChild(pName)
+    link.appendChild(pText)
+
+    p2.appendChild(link)
+    p.appendChild(p2)
+
+    elem.appendChild(p)
+}
+
+const compareStore = () => {
+    const categories = [storeStock[0][5], storeStock[1][5]]
+    const diff = categories.map((c,i,t) => {
+        const flat = t.filter(y => y !== c).reduce((a, z) => a.concat(z), [])
+        return c.filter(n => flat.filter(g => g === n).length<t.length-1)
+    })
+    getProductInformation(diff)
+}
+
+const addStock = stock => {
+    storeStock.push(stock)
+    if (storeStock.length === 2)
+        compareStore()
+}
+
+const getStock = storeId => {
+    $.get({
+        url: `stores/${storeId}.json`,
+        aync: false,
+        success: addStock
+    })
+}
+
+loadData()
+getStores()
+addEventListener()
